2477adc365127a742e30f8c924d1b4c06d9b1046
Edvins Antonovs <ummahusla@gmail.com>
2023-09-04 08:57:31 +0100

    feat: counter

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,20 @@
-import { screen, render } from '@testing-library/react';
+// @vitest-environment happy-dom
+
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { render } from './test/utilities';
+
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+});
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {},
-);
+test('it should increment when the "Increment" button is pressed', async () => {
+  const { user } = render(<Counter />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('0');
+  const button = screen.getByRole('button', { name: /increment/i });
+  await user.click(button);
+  expect(currentCount).toHaveTextContent('1');
+});
