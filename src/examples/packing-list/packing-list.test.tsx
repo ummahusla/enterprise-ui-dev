0cdcb029578b6e358b05f61273ec3d4932d16b60
Edvins Antonovs <ummahusla@gmail.com>
2023-09-07 08:58:40 +0100

    feat: added test

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from 'test/utilities';
+import { fireEvent, render, screen } from 'test/utilities';
 import PackingList from '.';
 
 it('renders the Packing List application', () => {
@@ -10,19 +10,36 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByLabelText('Add New Item');
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty', () => {
+  render(<PackingList />);
+  const input = screen.getByLabelText('Add New Item');
+  const button = screen.getByRole('button', { name: 'Add New Item' });
+  expect(input).toHaveValue('');
+  expect(button).toBeDisabled();
+});
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  render(<PackingList />);
+  const button = screen.getByRole('button', { name: 'Add New Item' });
+  expect(button).toBeDisabled();
+  const input = screen.getByLabelText('New Item Name');
+  fireEvent.change(input, { target: { value: 'test' } });
+  expect(input).toHaveValue('test');
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  render(<PackingList />);
+  const button = screen.getByRole('button', { name: 'Add New Item' });
+  expect(button).toBeDisabled();
+  const input = screen.getByLabelText('New Item Name');
+  fireEvent.change(input, { target: { value: 'test' } });
+  expect(input).toHaveValue('test');
+  fireEvent.click(button);
+  const list = await screen.findByTestId('unpacked-items-list');
+  expect(list).toHaveTextContent('test');
+});
