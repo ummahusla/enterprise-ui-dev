bf69de0d578945b23f5fc7c888d4e52b596c41ed
Edvins Antonovs <ummahusla@gmail.com>
2023-09-10 07:49:48 +0100

    fea: timezone

--- src/examples/time-zone/time-zone.test.tsx
+++ src/examples/time-zone/time-zone.test.tsx
@@ -1,6 +1,26 @@
-import { test, expect, vi } from 'vitest';
+import { test, expect, vi, beforeEach, afterAll } from 'vitest';
 import { render } from 'test/utilities';
 import TimeZone from '.';
+
+beforeEach(() => {
+  vi.useFakeTimers();
+  vi.setSystemTime(new Date('2021-01-01T00:00:00.000Z'));
+});
+
+afterAll(() => {
+  vi.useRealTimers();
+});
+
+test('it should render the current time', () => {
+  const date = new Date('2021-01-01T00:00:00.000Z');
+
+  vi.useFakeTimers();
+  vi.setSystemTime(date);
+
+  expect(Date.now()).toBe(date.valueOf());
+
+  vi.useRealTimers();
+});
 
 test('it should render successfully', () => {
   render(<TimeZone />);
